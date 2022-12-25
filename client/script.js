d2ae441e9d5d1e6679be157b4cf4c542f1b26241
Zhihan Tuo <johntuozh@gmail.com>
2022-12-25 07:03:25 -0800

    updated the server url

--- client/script.js
+++ client/script.js
@@ -79,15 +79,15 @@ const handleSubmit = async (e) => {
 
   // fetch data from server -> bot's response
 
-  const response = await fetch('http://localhost:5000', {
-    method: 'POST',
+  const response = await fetch("https://lil-bro.onrender.com", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      prompt: data.get('prompt')
-    })
-  })
+      prompt: data.get("prompt"),
+    }),
+  });
 
   clearInterval(loadInterval);
   messageDiv.innerHTML = '';
